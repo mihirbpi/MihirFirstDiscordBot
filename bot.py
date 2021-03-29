8c9a477df085547842f3774c7e8c838411f2651e
mihirbpi <mihir.borkar@gmail.com>
2021-03-29 10:46:16 -0700

    Added $yelp command

--- bot.py
+++ bot.py
@@ -2,11 +2,16 @@
 import os
 import random
 import discord
+import geocoder
+from yelpapi import YelpAPI
 from discord.ext import commands
 from dotenv import load_dotenv
+
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+YELP_API_KEY = os.getenv("YELP_API_KEY")
 bot = commands.Bot(command_prefix='$')
+yelp_api = YelpAPI(YELP_API_KEY)
 
 @bot.command(name='roll_dice', help='Simulates rolling dice.')
 async def roll(ctx, number_of_dice: int, number_of_sides: int):
@@ -16,4 +21,20 @@ async def roll(ctx, number_of_dice: int, number_of_sides: int):
     ]
     await ctx.send(', '.join(dice))
 
-bot.run(TOKEN)
+@bot.command(name='yelp', help="""Searches Yelp. Required parameters:\n
+                                search_text: text to search (put inside "" quotes if it has spaces),\n
+                                location: location (put inside "" quotes if it has spaces),\n
+                                n: Which result to return (returns nth result).""")
+
+async def yelp_query(ctx, search_text: str, location: str, n: int):
+    search_results = yelp_api.search_query(term=search_text, location=location)
+
+    if(int(n) > search_results['total']):
+        await ctx.send("not enough results")
+
+    else:
+        place = search_results['businesses'][int(n)-1]['url']
+        #await ctx.send('You passed {} and {}'.format(search_text, n))
+        await ctx.send(place)
+
+bot.run(DISCORD_TOKEN)
