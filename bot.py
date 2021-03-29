e9af96612b504858659ba1cb60607cdf1be51ae4
mihirbpi <mihir.borkar@gmail.com>
2021-03-29 10:56:29 -0700

    Spacing

--- bot.py
+++ bot.py
@@ -20,6 +20,7 @@ yelp_api = YelpAPI(YELP_API_KEY)
 
 # $roll_dice command
 @bot.command(name='roll_dice', help='Simulates rolling dice.')
+
 async def roll(ctx, number_of_dice: int, number_of_sides: int):
     dice = [
         str(random.choice(range(1, number_of_sides + 1)))
